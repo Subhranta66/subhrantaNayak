246351b857d9b6beb7e3149bd59d1023da9b47cd
zippercartoonist <94288115+MuhammadFaizanArif@users.noreply.github.com>
2022-06-06 11:19:14 +0500

    Update

--- bot/stuff.py
+++ bot/stuff.py
@@ -27,7 +27,7 @@ async def start(event):
 async def zylern(event):
     await event.reply(
         f"""
-**Available Commands 🤖**
+**Available Commands 😉**
 
 /start - __Check Bot is Working Or Not__
 /help - __Get Detailed Help__
@@ -39,9 +39,6 @@ async def zylern(event):
 /renew - __Clear Cached Downloads__
 /clear - __Clear Queued Files__
 /showthumb - __Show Current Thumbnail__
-/speed - __Do A SpeedTest__
-/eval - __Execute An Argument__
-/bash - __Run Bash Commands__
 /cmds - __List Available Commands__
 """
     )
@@ -49,10 +46,10 @@ async def zylern(event):
 
 async def help(event):
     await event.edit(
-        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
+        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
     )
     
 async def ihelp(e):
     await e.reply(
-        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
+        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
     )
